c2394e0adb16ed48f2858f9a17e1520a09ee139c
Deepthi1003 <dtchokka@gmail.com>
2019-10-11 16:07:39 -0500

    adding changes

--- code.test.js
+++ code.test.js
@@ -1,32 +1,43 @@
-QUnit.module('Main', {})
+QUnit.module('MAIN MODULE', {})  // group all these tests together
 
-QUnit.test( "Test calcCG", function( assert ) {
-    assert.equal(testcalc(5,2);
-    assert.equal(testcalc(9,-4);
-    assert.equal(testcalc(8,0);
-    assert.equal(testcalc(8,2);
-    assert.equal(testcalc(5,8);
-});
+QUnit.test('TEST add', assert => {
+  assert.equal(numbpeople(1, 1), 1, 'Positive integers')
+  assert.equal(numbpeople(-1, -1), 2, 'Positive integers')
+  assert.equal(numbpeople(10, 10), 100, 'valuess')
+})
+
+QUnit.config.autostart = false  // sync = false; start after loading html
 
 window.addEventListener('load', () => {
-    const appURL = '../index.html'
-    const openingTag = '<main class="container mt-5 flex-fill">'
-    const closingTag = '</main>'
-    fetch(appURL, { method: 'GET' })
-      .then(response => {
-        return response.text() // returns promise
-      })
-      .then(txt => {
-        const start = txt.indexOf(openingTag)
-        const end = txt.indexOf(closingTag) + closingTag.length
-        const html = txt.substring(start, end)
-        const qunitFixtureBody = document.getElementById('qunit-fixture')
-        qunitFixtureBody.innerHTML = html
-        console.info(qunitFixtureBody)
-        QUnit.start()
-      })
-      .catch(error => {
-        console.error('error:', error);
-        QUnit.start()
-      })
-  })
+  const appURL = '../index.html'
+  const openingTag = '<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">'
+  const closingTag = '</body>'
+  fetch(appURL, { method: 'GET' })
+    .then(response => {
+      return response.text() // returns promise
+    })
+    .then(txt => {
+      const start = txt.indexOf(openingTag)
+      const end = txt.indexOf(closingTag) + closingTag.length
+      const html = txt.substring(start, end)
+      const qunitFixtureBody = document.getElementById('qunit-fixture')
+      qunitFixtureBody.innerHTML = html
+      console.info(qunitFixtureBody)
+      QUnit.start()
+    })
+    .catch(error => {
+      console.error('error:', error);
+      QUnit.start()
+    })
+})
+
+QUnit.test("TEST first number validation", assert => {
+  const input = document.querySelector('#firstNumber')
+  const warning = document.querySelector('#firstWarning')
+  input.value = -3;
+  assert.equal(input.value, -3, "Bad value assigned")
+  assert.strictEqual(input.checkValidity(), false, "Correctly fails validation")
+  input.focus()
+  input.blur()
+  assert.strictEqual(warning.innerHTML, 'Invalid input', `Correctly adds warning ${warning}`)
+})
